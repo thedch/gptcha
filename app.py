a87c3704afbc1b999db72be73ae4c7e4d0bf65de
Daniel <13357734+thedch@users.noreply.github.com>
2023-04-13 09:45:00 -0700

    Ok try requests

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 import os
+from time import sleep
 import streamlit as st
 import streamlit.components.v1 as components
-
-from datetime import datetime
+import requests
 
 
 @st.cache_data
 def run_server():
     os.system('python myserver.py &')
+    sleep(1)
 
 def try_html():
     run_server()
 
     st.write('Iframe Begin')
+
+    r = requests.get('http://127.0.0.1:32100')
+    r.raise_for_status()
+    st.write(r.text)
     components.iframe("http://127.0.0.1:32100", width=800, height=800, scrolling=True)
     st.write('Iframe End')
 
