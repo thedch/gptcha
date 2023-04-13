6463c0ba20c134592d1881fc9211823eb61dd89c
Daniel <13357734+thedch@users.noreply.github.com>
2023-04-13 00:15:49 -0700

    Simplify, remove legacy frontend dir

--- app.py
+++ app.py
@@ -1,33 +1,13 @@
-import base64
 import streamlit as st
 import streamlit.components.v1 as components
 
 from datetime import datetime
 
-if 'counter' not in st.session_state:
-    st.session_state.counter = 0
-
-
-
-def main():
-    st.title('PDF Reader')
-    with open('./attn-small.pdf', "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
-    pdf_embed = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="400" height="1000" type="application/pdf"></iframe>'
-
-    st.markdown(pdf_embed, unsafe_allow_html=True)
-    st.write('Finished', base64_pdf[:100])
-
 
 @st.cache_data
 def run_server():
-    st.write('Running server...')
-    print('Starting...')
     import os
-    os.system('python frontend/myserver.py &')
-    st.write('Server started')
-    print('Returning...')
+    os.system('python myserver.py &')
 
 
 def try_html():
@@ -35,7 +15,7 @@ def try_html():
     run_server()
 
     st.write('Iframe Begin')
-    components.iframe("http://localhost:8000/frontend", width= 800, height=800, scrolling=True)
+    components.iframe("http://localhost:8000", width=800, height=800, scrolling=True)
     st.write('Iframe End')
 
 
@@ -61,7 +41,4 @@ def try_custom_component():
     handle_event(run_component(props))
 
 if __name__ == '__main__':
-    # main()
     try_html()
-    # try_custom_component()
-    # try_gpt4_custom_component()
